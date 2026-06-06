7f0d6579db25287c4a223312ce007548b6a2e239
Rolemberg <rolemberg.junior@gescorpgo.com>
2026-06-06 11:16:08 -0300

    feat: add SubscriptionGate to private layout

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import SubscriptionGate from "@/components/SubscriptionGate";
 
 export default function PrivateLayout({
   children,
@@ -7,7 +8,9 @@ export default function PrivateLayout({
 }>) {
   return (
     <>
-      <main className="md:pb-0 md:pt-20">{children}</main>
+      <SubscriptionGate>
+        <main className="md:pb-0 md:pt-20">{children}</main>
+      </SubscriptionGate>
       <Header />
     </>
   );
